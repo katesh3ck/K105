a014da30f283b8073e0dd1ae3896f3e2e9d8862e
katesh3ck <ermolenko3ck@gmail.com>
2024-12-17 22:14:53 +0300

    Запустился проект, но при нажатии кнопки все пошло по жопке

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module com.example.k105 {
-    requires java.sql;
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     exports com.example.k105.controllers;
-    exports com.example.k105.dao; // Экспорт пакета DAO
+    exports com.example.k105.dao;
+    exports com.example.k105.models;
 
+    opens com.example.k105.models to javafx.fxml; // Открываем для FXMLLoader
 }
