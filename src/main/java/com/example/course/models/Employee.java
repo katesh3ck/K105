8e628755f26ed092887a92d5d84a571f8b30743f
katesh3ck <ermolenko3ck@gmail.com>
2024-12-19 21:26:07 +0300

    Працуе, хочу убрать лишнее Убрала лишнее в Employee

--- src/main/java/com/example/course/models/Employee.java
+++ src/main/java/com/example/course/models/Employee.java
@@ -25,35 +25,11 @@ public class Employee {
     public int getId() {
         return id.get();
     }
-
-    public void setId(int id) {
-        this.id.set(id);
-    }
-
-    public IntegerProperty idProperty() {
-        return id;
-    }
-
-    public String getName() {
-        return name.get();
-    }
-
-    public void setName(String name) {
-        this.name.set(name);
-    }
-
+///!!!!!
     public StringProperty nameProperty() {
         return name;
     }
-
-    public String getDepartmentName() {
-        return departmentName.get();
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName.set(departmentName);
-    }
-
+///!!!!!
     public StringProperty departmentNameProperty() {
         return departmentName;
     }
@@ -62,32 +38,12 @@ public class Employee {
         return salary.get();
     }
 
-    public void setSalary(double salary) {
-        this.salary.set(salary);
-    }
-
-    public DoubleProperty salaryProperty() {
-        return salary;
-    }
-
-    public double getBonus() {
-        return bonus.get();
-    }
-
     public void setBonus(double bonus) {
         this.bonus.set(bonus);
     }
 
     public DoubleProperty bonusProperty() {
         return bonus;
-    }
-
-    public LocalDate getHireDate() {
-        return hireDate.get();
-    }
-
-    public void setHireDate(LocalDate hireDate) {
-        this.hireDate.set(hireDate);
     }
 
     public ObjectProperty<LocalDate> hireDateProperty() {
